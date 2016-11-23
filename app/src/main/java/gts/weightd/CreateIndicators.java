72bf8a8a0620cda3f75363392ed8ea2f6d1bdc74
agoodman42 <aarongoodman42@gmail.com>
2016-11-22 18:51:05 -0800

    Got 2 activities up and running, and tweaked the layout a tad. Created a spinner ( drop down ) for the indicator's activity. Next steps- create indicators, store them locally ( even once the app closes) and then share them between activities.

---
+++ app/src/main/java/gts/weightd/CreateIndicators.java
@@ -0,0 +1,30 @@
+package gts.weightd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateIndicators extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_indicators);
+
+
+        /*
+
+        http://stackoverflow.com/questions/4967740/transfer-data-from-one-activity-to-another-activity-using-intents
+        Use this stack overflow page to continue your work. Next steps are to create indicators and
+        a) figure out how to store them as persistent data
+        and
+        b) figure out how to pass them from activity to activity.
+
+        you're also going to need to figure out how to create a list view and then take the
+        results of this activity and add to an array which will populate that list-view.
+
+
+
+
+         */
+    }
+}
