b5ecb8566a14a976be999ee10f0049a858f96514
agoodman42 <aarongoodman42@gmail.com>
2016-11-23 23:35:04 -0800

    got the intent to push the color from color picker to create indicators, but cant change the drawable color for some reason

--- app/src/main/java/gts/weightd/ColorPickerActivity.java
+++ app/src/main/java/gts/weightd/ColorPickerActivity.java
@@ -1,56 +1,92 @@
 package gts.weightd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.larswerkman.holocolorpicker.ColorPicker;
 import com.larswerkman.holocolorpicker.OpacityBar;
 import com.larswerkman.holocolorpicker.SVBar;
-import com.larswerkman.holocolorpicker.SaturationBar;
-import com.larswerkman.holocolorpicker.ValueBar;
 
-import static gts.weightd.R.id.picker;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 
 public class ColorPickerActivity extends AppCompatActivity
-        implements ColorPicker.OnColorChangedListener {
+        implements ColorPicker.OnColorChangedListener, View.OnClickListener {
 
     private ColorPicker picker;
     private SVBar svBar;
     private OpacityBar opacityBar;
     private Button button;
-    private TextView text;
+    private Button submitButton;
+    private float color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
 
+
+
         picker = (ColorPicker) findViewById(R.id.picker);
         svBar = (SVBar) findViewById(R.id.svbar);
         opacityBar = (OpacityBar) findViewById(R.id.opacitybar);
-        button = (Button) findViewById(R.id.button1);
-        text = (TextView) findViewById(R.id.textView1);
+        button = (Button) findViewById(R.id.colorPickerChangeColorButton);
+        submitButton = (Button) findViewById(R.id.colorSubmitButton);
 
         picker.addSVBar(svBar);
         picker.addOpacityBar(opacityBar);
         picker.setOnColorChangedListener(this);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(this);
+        submitButton.setOnClickListener(this);
+        color = picker.getColor();
 
-            @Override
-            public void onClick(View v) {
-                text.setTextColor(picker.getColor());
-                picker.setOldCenterColor(picker.getColor());
-            }
-        });
+
     }
+
+
+
 
     @Override
     public void onColorChanged(int color) {
         //gives the color when it's changed.
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.colorPickerChangeColorButton:
+                picker.setOldCenterColor(picker.getColor());
+                break;
+
+
+            case R.id.colorSubmitButton:
+
+
+                Intent indicatorIntent = new Intent(ColorPickerActivity.this ,
+                        CreateIndicatorsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("changeColor", picker.getColor());
+                indicatorIntent.putExtras(bundle);
+                startActivity(indicatorIntent);
+
+                break;
+
+            default:
+                break;
+
+
+
+        }
+    }
 }
+
+//http://stackoverflow.com/questions/4967740/transfer-data-from-one-activity-to-another-activity-using-intents
--- app/src/main/java/gts/weightd/CreateIndicatorsActivity.java
+++ app/src/main/java/gts/weightd/CreateIndicatorsActivity.java
@@ -1,21 +1,30 @@
 package gts.weightd;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import static gts.weightd.R.id.circleButton;
+
 public class CreateIndicatorsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int color;
+    Button colorButton;
+    Bundle extras;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_indicators);
-
-        Button colorButton = (Button) findViewById(R.id.circleButton);
+        Button colorButton = (Button) findViewById(circleButton);
         colorButton.setOnClickListener(this);
+
+
 
 
         /*
@@ -37,12 +46,21 @@ public class CreateIndicatorsActivity extends AppCompatActivity implements View.
 
         switch (v.getId()) {
 
-            case R.id.circleButton:
+            case circleButton:
                 Intent intent = new Intent(this, ColorPickerActivity.class);
 //                //intent.putExtra(EXTRA_MESSAGE, message);
                 startActivity(intent);
-                Toast.makeText(getApplicationContext(), "circle button works",
-                        Toast.LENGTH_SHORT).show();
+
+
         }
     }
-}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent i = this.getIntent();
+        int id = i.getIntExtra("changeColor", 000000);
+        Toast.makeText(this, "" + id, Toast.LENGTH_SHORT).show();
+        }
+
+    }
